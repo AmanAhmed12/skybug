6bb8fc45c7ca97d4b05a7389e504b1eede307d2c
Aman Ahmed <122817669+AmanAhmed12@users.noreply.github.com>
2024-03-03 16:30:51 +0530

    Random Number Game
    Task 1

---
+++ numberGame/src/main/java/com/mycompany/numbergame/NumberGame.java
@@ -0,0 +1,11 @@
+
+
+package com.mycompany.numbergame;
+
+public class NumberGame {
+
+    public static void main(String[] args) {
+       Game g1=new Game();
+       g1.setVisible(true);
+    }
+}
